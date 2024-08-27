8bd23fbc99ab4560dc8105b60ea85e47f266b7fa
AlejandraGomezH <93231057+AlejandraGomezH@users.noreply.github.com>
2024-08-27 16:09:17 -0600

    Update código

---
+++ app.js
@@ -0,0 +1,37 @@
+let textoEncriptadoGlobal = '';
+
+function asignarTextoElemento(elemento, texto){
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+
+function encriptarTexto() {
+
+    let textoUsuario = document.getElementById('valorUsuario').value;
+    let textoEncriptado = '';
+
+    console.log(typeof textoUsuario );
+    for (let i = 0; i < textoUsuario.length; i++) {
+        textoEncriptado += String.fromCharCode(textoUsuario.charCodeAt(i) + 1);
+    }
+
+    textoEncriptadoGlobal = textoEncriptado;
+    asignarTextoElemento('#textoEncriptado', textoEncriptado);
+
+    console.log(textoEncriptado);
+    //console.log(typeof(textoEncriptado));
+    return textoEncriptado;
+}
+
+function desencriptarTexto() {
+    let textoDesencriptado = '';
+
+    for (let i = 0; i < textoEncriptadoGlobal.length; i++) {
+        textoDesencriptado += String.fromCharCode(textoEncriptadoGlobal.charCodeAt(i) - 1);
+    }
+    asignarTextoElemento('#textoEncriptado', textoDesencriptado);
+    return textoDesencriptado;
+}
+
+
